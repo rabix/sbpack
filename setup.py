247076682fa6165909b6bba2979fd0e592417fd2
Kaushik Ghose <kaushik.ghose@gmail.com>
2020-02-15 09:17:49 -0500

    Relax python requirement to >= 3.5

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     version=version,
     packages=find_packages(),
     platforms=['POSIX', 'MacOS', 'Windows'],
-    python_requires='>=3.7.0',
+    python_requires='>=3.5.0',
     install_requires=[
         "ruamel.yaml >= 0.15.77",
         "sevenbridges-python >= 0.20.2"
