bc3afa633575867c94c63a1de64c023fbfe9a891
Kaushik Ghose <kaushik.ghose@gmail.com>
2020-03-12 18:06:25 -0400

    Add tests for step packing and remote packing

--- tests/test_packing.py
+++ tests/test_packing.py
@@ -33,3 +33,21 @@ def test_schema_def2():
     _type = cwl.get("inputs").get("in2").get("type")
     assert isinstance(_type, dict)
     assert _type.get("type") == "enum"
+
+
+def test_step_packing():
+    cwl = pack("remote-cwl/wf1.cwl")
+    s1 = cwl.get("steps").get("s1")
+    tool2 = s1.get("run")
+    _type = tool2.get("inputs").get("in1").get("type")
+    assert isinstance(_type, dict)
+    assert _type.get("type") == "array"
+
+
+def test_remote_packing():
+    cwl = pack("https://raw.githubusercontent.com/kaushik-work/sbpack/master/tests/wf2.cwl")
+    s1 = cwl.get("steps").get("s1")
+    tool2 = s1.get("run")
+    _type = tool2.get("inputs").get("in1").get("type")
+    assert isinstance(_type, dict)
+    assert _type.get("type") == "array"
