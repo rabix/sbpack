9ef7a18350963d1016f0b84849a19333386f89a7
Kaushik Ghose <kaushik.ghose@gmail.com>
2020-09-17 17:00:27 -0400

    Add test for pulling

---
+++ tests/test_pulling.py
@@ -0,0 +1,13 @@
+import subprocess
+import pathlib
+import tempfile
+
+
+def test_pull():
+
+    with tempfile.TemporaryDirectory() as td:
+        out_dir = pathlib.Path(td)
+        subprocess.run(["sbpull", "--unpack", ".", "admin/sbg-public-data/eqtl-analysis-with-fastqtl-gtex-v7/", str(out_dir / "eqtl.cwl")], check=True)
+
+        assert (out_dir / "eqtl.cwl").exists()
+        assert (out_dir / "eqtl.cwl.steps").exists()
